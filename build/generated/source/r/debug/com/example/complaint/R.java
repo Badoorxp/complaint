991cf966386c5c3ed4b99bf71177979644a05319
MahmoudAlSharaya <mahmoudalsharayaa@yahoo.com>
2015-11-08 18:12:41 +0200

    اضافة كود الكاميرا

--- build/generated/source/r/debug/com/example/complaint/R.java
+++ build/generated/source/r/debug/com/example/complaint/R.java
@@ -56,16 +56,21 @@ public final class R {
         public static final int action_example=0x7f060000;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060002;
-        public static final int hello_world=0x7f060003;
-        public static final int navigation_drawer_close=0x7f060004;
-        public static final int navigation_drawer_open=0x7f060005;
-        public static final int title_activity_finish=0x7f060006;
-        public static final int title_activity_newcomp=0x7f060007;
-        public static final int title_activity_newcomplaint=0x7f060008;
-        public static final int title_activity_selectimage=0x7f060009;
-        public static final int title_section1=0x7f06000a;
-        public static final int title_section2=0x7f06000b;
-        public static final int title_section3=0x7f06000c;
+        public static final int fin_b1=0x7f060003;
+        public static final int fin_b2=0x7f060004;
+        public static final int fin_b3=0x7f060005;
+        public static final int fin_b4=0x7f060006;
+        public static final int fin_text1=0x7f060007;
+        public static final int hello_world=0x7f060008;
+        public static final int navigation_drawer_close=0x7f060009;
+        public static final int navigation_drawer_open=0x7f06000a;
+        public static final int title_activity_finish=0x7f06000b;
+        public static final int title_activity_newcomp=0x7f06000c;
+        public static final int title_activity_newcomplaint=0x7f06000d;
+        public static final int title_activity_selectimage=0x7f06000e;
+        public static final int title_section1=0x7f06000f;
+        public static final int title_section2=0x7f060010;
+        public static final int title_section3=0x7f060011;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
--- src/com/example/complaint/Selectimage.java
+++ src/com/example/complaint/Selectimage.java
@@ -2,22 +2,31 @@ package com.example.complaint;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Selectimage extends Activity {
-
+	Button takephoto;
+	ImageView photo;
+	public static final int CAM_REQUEST = 1313;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_selectimage);
+		 takephoto = (Button)findViewById(R.id.newc);
+		photo = (ImageView)findViewById(R.id.take);
 		Button send=(Button)findViewById(R.id.exitapp);
 		Button change=(Button)findViewById(R.id.newcom);
 		Button exit=(Button)findViewById(R.id.exit);
+		photo.setOnClickListener(new takephotoClicker());
+
 		send.setOnClickListener(new OnClickListener(){
 
 			@Override
@@ -56,22 +65,19 @@ public class Selectimage extends Activity {
 		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.selectimage, menu);
-		return true;
-	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode==CAM_REQUEST){
+			Bitmap th=(Bitmap) data.getExtras().get("data");
+			photo.setImageBitmap(th);
 		}
-		return super.onOptionsItemSelected(item);
 	}
+	class takephotoClicker implements OnClickListener{
+		@Override
+		public void onClick(View v) {
+			Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(cameraintent,CAM_REQUEST);
+	}}
 }
