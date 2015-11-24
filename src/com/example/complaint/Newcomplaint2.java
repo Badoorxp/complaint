a6a084fd634778fccf5ad456aff53771f15b6ad0
MahmoudAlSharaya <mahmoudalsharayaa@yahoo.com>
2015-11-24 20:01:00 +0200

    تغيير شاشة البداية

--- src/com/example/complaint/Newcomplaint2.java
+++ src/com/example/complaint/Newcomplaint2.java
@@ -8,9 +8,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 public class Newcomplaint2 extends Activity {
+    EditText name = (EditText) findViewById(R.id.editText);
+    EditText phone = (EditText) findViewById(R.id.editText2);
+    Spinner city = (Spinner) findViewById(R.id.spinner);
+    EditText location = (EditText) findViewById(R.id.editText3);
+    EditText ditals = (EditText) findViewById(R.id.editText4);
+    String typycomp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,17 @@ public class Newcomplaint2 extends Activity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Newcomplaint2.this, Selectimage.class);
+                String name1 = name.getText().toString();
+                String phone1 = phone.getText().toString();
+                String location1 = location.getText().toString();
+                String ditalse1 = ditals.getText().toString();
+                String city1 = city.getSelectedItem().toString();
+                i.putExtra("typecomp",typycomp);
+                i.putExtra("location", location1);
+                i.putExtra("phone", phone1);
+                i.putExtra("name", name1);
+                i.putExtra("ditalse", ditalse1);
+                i.putExtra("city", city1);
                 startActivity(i);
 
             }
@@ -40,18 +60,43 @@ public class Newcomplaint2 extends Activity {
 
 
             public void onItemSelected(AdapterView<?> parent, View view,
-                                       int pos, long id)
-            {
+                                       int pos, long id) {
                 // An item was selected. You can retrieve the selected item using
                 // parent.getItemAtPosition(pos)
             }
 
-            public void onNothingSelected(AdapterView<?> parent)
-            {
+            public void onNothingSelected(AdapterView<?> parent) {
                 // Another interface callback
             }
+
+        }}
+//ميثود نوع الشكوى
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.radioButton5:
+                if (checked)
+                    typycomp = "نفايات";
+                break;
+            case R.id.radioButton:
+                if (checked)
+                    typycomp = "مياه";
+                break;
+            case R.id.radioButton3:
+                if (checked)
+                    typycomp = "صرف صحي";
+                break;
+            case R.id.radioButton6:
+                if (checked)
+                    typycomp = "اخرى";
+                break;
 
         }
 
     }
 }
+
